a2f9ea0c2234452492ba3ccfc3150eee9984f966
ADRIANA M. RIOS <amrios@us.ibm.com>
2016-04-23 12:10:15 -0500

    adding login - not working yet

--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 import hashlib
 import datetime
 from init import app, db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(20))
+    email = db.Column(db.String(100), unique=True)
+    pw_hash = db.Column(db.String(64))
+    displayName = db.Column(db.String(40))
+
+
+    @property
+    def grav_hash(self):
+        hash = hashlib.md5()
+        hash.update(self.email.strip().lower().encode('utf8'))
+        return hash.hexdigest()
+
+
+    @property
+    def jsonable(self):
+        return {
+            'id': self.id,
+            'grav_hash': self.grav_hash,
+            'name': self.name
+        }
--- views.py
+++ views.py
@@ -6,3 +6,9 @@ from sqlalchemy.orm import joinedload
 from init import app, db
 from sqlalchemy import or_
 from sqlalchemy import desc
+
+
+@app.route('/user/<int:uid>')
+def show_user(uid):
+    user = models.User.query.get_or_404(uid)
+    return flask.render_template('user.html')
