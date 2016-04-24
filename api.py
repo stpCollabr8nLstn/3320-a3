e648ce5f8f2c33cfa9b8425f8b123278954f4b4c
ADRIANA M. RIOS <amrios@us.ibm.com>
2016-04-23 19:34:08 -0500

    styling

--- api.py
+++ api.py
@@ -4,3 +4,47 @@ import flask_socketio as sio
 from init import app, db, socketio
 import models
 
+
+def check_request():
+    token = flask.session['csrf_token']
+    if flask.request.form['_csrf_token'] != token:
+        app.logger.warn('invalid CSRF token')
+        flask.abort(400)
+    if flask.session.get('auth_user') != int(flask.request.form['creator_id']):
+        app.logger.warn('requesting user %s not logged in (%s)',
+                        flask.request.form['creator_id'],
+                        flask.session.get('auth_user'))
+        flask.abort(403)
+
+
+@app.route('/msg/send', methods=['POST'])
+def handle_send():
+    check_request()
+    init = flask.g.user.id
+    other = int(flask.request.form['recipient_id'])
+    msg = send_message(init, other)
+    # notify the recipient of the hai
+    # emit an event of type 'hai', with msg content, to all connections
+    # in the recipient's room
+    socketio.emit('hai', msg.jsonable, room='user-{}'.format(other))
+    return flask.jsonify(msg.jsonable)
+
+
+@socketio.on('connect')
+def on_connect():
+    # get the connecting user's user ID
+    # flask.session works in socket IO handlers :)
+    uid = flask.session.get('auth_user', None)
+    if uid is None:
+        app.logger.warn('received socket connection from unauthed user')
+        return
+
+    app.logger.info('new client connected for user %d', uid)
+
+    # add this connection to the user's 'room', so we can send to all
+    # the user's open browser tabs
+    join_room('user-{}'.format(uid))
+
+@socketio.on('disconnect')
+def on_disconnect():
+    app.logger.info('client disconnected')
--- static/chat.js
+++ static/chat.js
@@ -1,3 +1,44 @@
 /**
  * Created by Adriana on 4/23/16.
  */
+// Set up SocketIO
+var socket = io();
+
+function scrollChat() {
+    // a jQuery result is a list, get the first element
+    var chat = $('#messages')[0];
+    // figure out the height the *top* of window should scroll to
+    var pos = chat.scrollHeight - chat.clientHeight;
+    chat.scrollTop = pos;
+}
+
+/* Listen to new chats on the form */
+$('#chat').on('submit', function(event) {
+    // don't really submit the form
+    // we handle in JS, a real submit would reload page (sad)
+    event.preventDefault();
+    // get the message - it's the value of the message element
+    var msg = $('#m').val();
+    // send the message to the server
+    socket.send(msg);
+    // reset the message input
+    $('#m').val('');
+    // add the message to the end of our list
+    // most jQuery operations return the 'jquery' object they were
+    // called on
+    /* This says:
+       Append to '#messages' the results of
+       1. building an 'li' element
+       2. adding 'sent' to its 'class' attribute
+       3. setting its text to the message content
+     */
+    $('#messages').append($('<li>').addClass('sent').text(msg));
+    scrollChat();
+});
+
+/* listen for replies */
+socket.on('message', function(msg) {
+    // add it to the list!
+    $('#messages').append($('<li>').addClass('received').text(msg));
+    scrollChat();
+});
